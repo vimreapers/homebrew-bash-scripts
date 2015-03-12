235f0ef6fe5d94f2c439ef85234256ad0555be89
maxbrisance <cody.l.cochran@gmail.com>
2015-03-12 14:29:41 -0500

    Renamed files to new naming convention.

---
+++ sh-iusinstaller.rb
@@ -0,0 +1,12 @@
+require 'formula'
+
+class Iusinstaller < Formula
+  homepage 'http://github.com/vimreapers'
+  url 'https://github.com/vimreapers/homebrew-shell.git'
+  
+  depends_on "bash-completion" => :recommended
+
+  def install
+    bin.install 'sh-iusinstaller'
+  end
+end
